8dc0a5f0e59fb08d97fdc8347cde6b52398ef1c0
vadim <vadimlipakov@gmail.com>
2023-04-23 22:06:55 +0300

    Added Retrofit.

---
+++ app/src/main/java/com/lipakov/smartlink/model/UserSl.java
@@ -0,0 +1,22 @@
+package com.lipakov.smartlink.model;
+
+public class User {
+    private String email;
+    private String login;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+}
